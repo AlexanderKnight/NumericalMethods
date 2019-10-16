d871073a3905263786fce45c5bb98fb02d73a6a0
Alex Knight <AlexanderLKnight@protonmail.com>
2019-10-16 11:09:30 -0400

    Updated plotting programs for this homework

--- Homework3/convergence_check.py
+++ Homework3/convergence_check.py
@@ -5,22 +5,30 @@ from scipy.optimize import curve_fit
 def power_function(y, a, b, c):
     return a*(y**b)+c
 
+def linear_func(y,a,b):
+    return a*y+b
+
 
 method = "RungeKutta3-SineWave"
-cf = "0.500000"
-exts = np.array((100,200,400,800,1600))
+cf = "0.100000"
+exts = np.array((100,200,400,800))
+dx = np.array((2*np.pi)/exts)
 x = np.linspace(100,1600,1000)
 maxdiff = np.zeros(len(exts))
-extents = np.array(("100","200","400","800","1600"))
+extents = np.array(("100","200","400","800"))
 for i in range(len(exts)):
     dat = np.genfromtxt("data_files/"+method+"-cf-"+str(cf)+"-"+str(exts[i])+".dat")
     sin = np.genfromtxt("data_files/Sinewave-cf-"+str(cf)+"-"+str(exts[i])+".dat")
-    diff = np.abs(dat[:,2:-2]-sin);
-    maxdiff[i]=np.amax(diff)
+    print(len(dat[:,0]))
+    diff = np.abs(dat[-1,2:-2]-sin[-1,:]);
+    print(exts[i])
+    print(np.sum(diff)*dx[i])
+    maxdiff[i]=np.sum(diff)*dx[i]
+    
 
-opt,cov = curve_fit(power_function,exts,maxdiff,[1,-3,0])
+opt,cov = curve_fit(power_function,exts,maxdiff,[1,-4,0])
 plt.plot(exts,maxdiff)
-plt.plot(x,power_function(x,opt[0],opt[1],opt[2]),label="ax^b+c:\na={0}\nb={1}\nc={2}".format(opt[0],opt[1],opt[2]))
+plt.plot(x,power_function(x,opt[0],opt[1],opt[2]),label="a*(x**b)+c:\na={0}\nb={1}\nc={2}".format(opt[0],opt[1],opt[2]))
 plt.legend(bbox_to_anchor=(1,1))
 #plt.show()
 plt.savefig(method+"_ConvergenceTest.png",dpi=200)
--- Homework3/one_d_plotter.py
+++ Homework3/one_d_plotter.py
@@ -3,14 +3,16 @@ import matplotlib.pyplot as plt
 
 
 
-sine_data = np.genfromtxt("data_files/Sinewave-cf-0.500000.dat")
-data = np.genfromtxt("data_files/RungeKutta3-SineWave-cf-0.500000.dat")
-for i in range(len(data))[0::50]:
-    print("            "+str(i))
-    plt.figure(figsize=(3,3))
-    plt.title("RK3,3Upstream,0.5")
-    plt.plot(data[i,1:-1])
-    plt.plot(sine_data[i,:])
-    plt.savefig("figures/RungeKutta3_upstream_0.5_"+str(i)+".png", dpi=200)
-    plt.close()
+extents = ["100","200","400","800"]
+for j in range(len(extents)):
+    sine_data = np.genfromtxt("data_files/Sinewave-cf-0.100000-{0}.dat".format(extents[j]))
+    data = np.genfromtxt("data_files/RungeKutta3-SineWave-cf-0.100000-{0}.dat".format(extents[j]))
+    for i in range(len(data)-1)[0::50]:
+        print("            "+str(i))
+        plt.figure(figsize=(3,3))
+        plt.title("RK3,3Upstream,0.5\n{0},{1}".format(extents[j],i))
+        plt.plot(data[i,2:-2])
+        plt.plot(sine_data[i+1,:])
+        plt.savefig("figures/RungeKutta3_upstream_0.5_{0}_{1}.png".format(extents[j],i), dpi=200)
+        plt.close()
 
